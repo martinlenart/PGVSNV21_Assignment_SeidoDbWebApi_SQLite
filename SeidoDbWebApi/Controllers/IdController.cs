f84a24ace7f5e460fd0467687ecf15e0e76bd875
Martin <martin_lenart@icloud.com>
2022-08-17 10:59:29 +0200

    Initial commit

---
+++ SeidoDbWebApi/Controllers/IdController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+using SeidoDbWebApi.Logger;
+
+namespace DbAppWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class IdController : ControllerBase
+    {
+        private ILogger<LogController> _logger;
+
+        //GET /Log
+        [HttpGet()]
+        [ProducesResponseType(200, Type = typeof(string))]
+        public string Get([FromServices] ILoggerProvider myLogger)
+        {
+            return AppConfig.ThisConnection;
+        }
+        public IdController(ILogger<LogController> logger)
+        {
+            _logger = logger;
+            _logger.LogInformation($"IdController started: {AppConfig.ThisConnection}");
+        }
+    }
+}
